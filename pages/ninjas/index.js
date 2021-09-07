11d62be62f926e3a8c8340f3629125eddc937d96
exxnnonymous <sulavbista51@gmail.com>
2021-09-07 16:40:00 +0545

    initial commit

---
+++ pages/ninjas/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head"
+import Link from "next/link"
+import styles from '../../styles/Ninjas.module.css'
+
+
+const Ninjas = ({ninjas}) => {
+    return ( 
+        <>
+        <Head>
+          <title>Ninja List | Ninjas</title>
+          <meta name="keywords" content="ninjas"/>
+        </Head><div>
+        <h1>All Ninjas</h1>
+        {ninjas.map(ninja=>(
+            <Link href={'/ninjas/'+ ninja.id} key={ninja.id}>
+                <a className={styles.single}>
+                    <h3>{ninja.name}</h3>
+                </a>
+            </Link>
+        ))}
+    </div>
+    </> );
+}
+ 
+export default Ninjas;
+
+
+export const getStaticProps = async() =>{
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+        props: {ninjas:data}
+    }
+}
